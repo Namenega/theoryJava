8fbca61b2ec968bc30587d8b6b25e3a82b590d1d
namenega <mene.nath@protonmail.com>
2022-04-04 15:27:24 +0200

    rd

--- algorithm/findPrimeNumbers.java
+++ algorithm/findPrimeNumbers.java
@@ -2,15 +2,20 @@ package theoryJava.algorithm;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 // A prime number is a whole number greater than 1 that has only two factors:
 // 1 and the number itself.
 
 public class findPrimeNumbers {
     public static void main(String[] args) {
+        // Get input from client
+        System.out.println("Please input the limit: ");
+        Scanner scan = new Scanner(System.in);
+        int limit = scan.nextInt();
         List<Integer>   primeNumbers = new ArrayList<>();
 
-        for (int numberToCheck = 2; numberToCheck <= 1000; numberToCheck++) {
+        for (int numberToCheck = 2; numberToCheck <= limit; numberToCheck++) {
             boolean isPrime = true;
             for (int factor = 2; factor <= (numberToCheck / 2); factor++) {
                 if ((numberToCheck % factor) == 0) {
@@ -22,7 +27,7 @@ public class findPrimeNumbers {
                 primeNumbers.add(numberToCheck);
             }
         }
-        System.out.println("Prime numbers from 1 to 1000 are:\n");
+        System.out.println("Prime numbers from 1 to " + limit + " are:\n");
         for (int number : primeNumbers) {
             System.out.println(number);
         }
